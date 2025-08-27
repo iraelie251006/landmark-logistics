93f1e31ee238fd23a259750d7e54e4444861a404
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-27 18:42:03 +0200

    Implemented mini about, services, and team pages.

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <h1 className='text-5xl flex justify-center'>About Us</h1>
+    </section>
+  )
+}
+
+export default page
