dbc7b74a7c50ab9d1a0f39a3a81b4dd778cbf6ae
Elie <niyubwayoiraelie5777@gmail.com>
2025-09-01 15:23:09 +0200

    Added imageURL prop to TeamMember component and updated TeamContent component to use it & display them.

--- components/TeamContent.tsx
+++ components/TeamContent.tsx
@@ -12,16 +12,19 @@ const TeamContent = () => {
         title="Managing Director" 
         phone="+250788465059"
         isManager
+        imageURL='/partners/Theophile.jpeg'
       />
       <TeamMember 
         name="KARIRENGE Prudencienne" 
         title="Operations" 
         phone="+250788688342"
+        imageURL='/partners/Prudencienne.jpeg'
       />
       <TeamMember 
         name="IKUZWE THIERRY" 
         title="Operations" 
         phone="+250780463394"
+        imageURL='/partners/Thierry.jpeg'
       />
     </OfficeSection>
 
@@ -42,6 +45,7 @@ const TeamContent = () => {
         title="Branch Manager" 
         phone="+250787250050"
         isManager
+        imageURL='/partners/Bemeriki.jpeg'
       />
     </OfficeSection>
 
@@ -95,11 +99,13 @@ const TeamContent = () => {
         name="Maître Natukunda Patience" 
         title="Legal Advisor" 
         phone="+250784397240"
+        imageURL='/partners/Patience.jpeg'
       />
       <TeamMember 
         name="Maître Masengesho Alliance" 
         title="Legal Advisor" 
         phone="+250791760887"
+        imageURL='/partners/Alliance.jpeg'
       />
     </OfficeSection>
   </section>
--- components/TeamMember.tsx
+++ components/TeamMember.tsx
@@ -2,24 +2,31 @@ import { Card, CardContent } from "@/components/ui/card";
 import { MessageCircle, Phone, User } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 interface TeamMemberProps {
   name: string;
   title: string;
   phone: string;
   isManager?: boolean;
+  imageURL?: string;
 }
 
-export const TeamMember = ({ name, title, phone, isManager = false }: TeamMemberProps) => {
+export const TeamMember = ({ name, title, phone, isManager = false, imageURL }: TeamMemberProps) => {
   return (
     <Card className="group relative overflow-hidden bg-gradient-to-br from-card to-corporate-gray-light shadow-[var(--shadow-card)] hover:shadow-[var(--shadow-professional)] transition-[var(--transition-smooth)] border-border max-sm:border-corporate-blue/30 hover:border-corporate-blue/30">
       <CardContent className={`p-6 ${isManager ? "bg-accent" : "bg-accent/50"}`}>
         <div className="flex items-start space-x-4">
-          <div className={`flex-shrink-0 w-12 h-12 rounded-full flex items-center justify-center ${
+          {imageURL ? (
+            <Image src={imageURL} alt={name} width={48} height={48} className="rounded-full object-cover"/>
+          ) : (
+            <div className={`flex-shrink-0 w-12 h-12 rounded-full flex items-center justify-center ${
             isManager ? 'bg-corporate-blue text-primary-foreground' : 'bg-corporate-gray-light text-corporate-gray'
           }`}>
             <User className="w-6 h-6" />
           </div>
+          )}
+          
           <div className="flex-1 min-w-0">
             <h3 className={`font-semibold text-lg mb-1 ${
               isManager ? 'text-corporate-blue' : 'text-foreground'
